0d7a458132c7198c78eea901107b66485bdae2a8
Hoang <2251120349@ut.edu.vn>
2024-07-22 21:53:27 +0700

    UpAnhHoanChinh

--- fontend/pages/ThemSanPham.php
+++ fontend/pages/ThemSanPham.php
@@ -1,4 +1,6 @@
 <?php
+    include 'UploadFile.php';
+
     require('../../db/connect.php');
     if(isset($_POST['btn-reg'])){
         $proname = $_POST['proname'];
@@ -7,17 +9,16 @@
         $prostock = $_POST['prostock'];
         $imagePath = $_FILES['image_path']['name'];
         $Description = $_POST['prodescription'];
-        
+        $targetDir = "../../UploadImage/";
+        $imagePath = Uploads($_FILES['image_path'], $targetDir);
 
-        
-        echo "Name: $proname <br> Price: $proprice <br> Category: $catid <br> Stock: $prostock <br> Image path: $imagePath <br> Description: $Description";
         $addSql = "INSERT INTO product(proname, proprice, catid, prostock, image_path, prodescription) VALUES(?, ?, ?, ?, ?, ?)";
         $stmt = $conn->prepare($addSql);
         if($stmt){
             $stmt->bind_param("sdiiss", $proname, $proprice, $catid, $prostock, $imagePath, $Description);
             $stmt->execute();
+
             if($stmt->affected_rows > 0){
-                move_uploaded_file($_FILES['image_path']['tmp_name'], "../../UploadImage".$_FILES['image_path']['name']);
                 header("Location: ../../assets/fontend/pages/Product/ProductList.php");
             }else{
                 echo "Add failed";
