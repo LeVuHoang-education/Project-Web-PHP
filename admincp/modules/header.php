9f52bd234156f0b980fbb9046f4731cfbb788670
Hoang <2251120349@ut.edu.vn>
2024-07-23 21:45:53 +0700

    Admin page + bo sung them chuc nang form

--- admincp/modules/header.php
+++
@@ -1,1 +0,0 @@
-<p>Header admin pages</p>
---
+++ adminpanel/includes/aside.php
@@ -0,0 +1,14 @@
+<aside>
+    <div class="aside">
+        <div class="aside-header">
+            <h3>Quản lý</h3>
+        </div>
+        <div class="aside-body">
+            <ul>
+                <li><a href="../pages/dashboard">dasboard</a> </li>
+                <li><a href="../pages/contact.php">contact</a></li>
+                <li><a href="about.php">about</a></li>
+            </ul>
+        </div>
+    </div>
+</aside>
