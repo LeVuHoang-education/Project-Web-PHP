2dd00b28ce8d7a3da52f2efd041b9ca2a2550909
caonhathao <caonhathao2372004@gmail.com>
2024-08-03 09:54:59 +0700

    update navigate navbar

--- assets/frontend/pages/Product/product.php
+++ assets/frontend/pages/Product/product.php
@@ -4,30 +4,24 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../Component/Header/Header.css" />
-  <link rel="stylesheet" href="../Component/Footer/Footer.css" />
-  <link rel="stylesheet" href="../Component/ShowProduct/ShowProduct.css" />
+  <link rel="stylesheet" href="../../component/header/header.css" />
+  <link rel="stylesheet" href="../../component/Footer/Footer.css" />
+  <link rel="stylesheet" href="../../component/ShowProduct/ShowProduct.css" />
   <link rel="stylesheet" href="./product.css" />
   <title>Product</title>
 </head>
 
 <body>
   <?php
-  include '../Component/Header/Header.php';
+  include '../../component/header/header.php';
   ?>
   <div class="container">
 
     <?php
-    if (isset($_POST['cat_choose'])) {
-      $cat_number = $_POST['cat_choose'];
-    } else {
-      // Xử lý trường hợp không có giá trị, ví dụ:
-      $cat_number = 0; // hoặc một giá trị mặc định khác
-    }
-    include "../Component/ShowProduct/showProduct.php";
+    include "../../component/ShowProduct/showProduct.php";
     ?>
   </div>
-  <?php include "../Component/Footer/Footer.php" ?>
+  <?php include "../../component/Footer/Footer.php" ?>
 </body>
 
 </html>
