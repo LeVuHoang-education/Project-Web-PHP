c24c77946ae70812c0d60090977c4d6ec30372c2
caonhathao <73047910+caonhathao@users.noreply.github.com>
2024-08-05 18:51:17 +0700

    update

--- assets/frontend/pages/home/home.php
+++ assets/frontend/pages/home/home.php
@@ -5,38 +5,94 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="home.css" />
+  <link rel="stylesheet" href=" ../../component/productCard/productCard.css" />
   <link rel="stylesheet" href="../../component/header/header.css" />
   <link rel="stylesheet" href="../../component/Footer/Footer.css" />
-  <!-- <link rel="stylesheet" href="../../component/productCard/productCard.css" /> -->
+  <!-- <script src="../../component/header/header.js"></script> -->
   <title>Trang chủ</title>
 </head>
 
 <body>
-
   <?php
-  include "../../component/header/header.php"; 
+  include "../../component/header/header.php";
   ?>
 
   <div class="container">
     <!--ads-->
     <div class="ads">
       <a href="#">
-        <img class="ads-img" style="float:left;" src="../../src/Home/ads/2.jpg" alt="ads" />
+        <img class="ads-img" style="float:left;" src="../../src/Home/ads/2015.png" alt="ads" />
       </a>
     </div>
     <!--main content-->
     <div class="main-container">
-    </div>
+      <?php
+      require('../../../../db/connect.php');
+      function renderProductCard()
+      {
+        include "../../component/productCard/productCard.php";
+      }
+      function categoryID()
+      {
+        global $category;
+        return $category;
+      }
 
-    <!--ads-->
-    <div class="ads">
-      <a href="#">
-        <img class="ads-img" src="../../src/Home/ads/2.jpg" alt="ads" />
-      </a>
+      $minPrice = 0;
+      $maxPrice = 0;
+
+      function getMinPrice()
+      {
+        global $minPrice;
+        return $minPrice;
+      }
+      function getMaxPrice()
+      {
+        global $maxPrice;
+        return $maxPrice;
+      }
+      function getProductByRange($min, $max, $conn)
+      {
+        global $category;
+        $category = 0;
+        global $minPrice;
+        $minPrice = $min;
+        global $maxPrice;
+        $maxPrice = $max;
+        renderProductCard();
+      }
+
+      ?>
+
+      <div class="content">
+        <h1>Sản phẩm trên 10 triệu</h1>
+        <?php
+        getProductByRange(10000000, 99999999, $conn);
+        ?>
+      </div>
+      <div class="content">
+        <h1>Sản phẩm trên 5 triệu</h1>
+        <?php
+        getProductByRange(5000000, 9999999, $conn);
+        ?>
+      </div>
+      <div class="content">
+        <h1>Sản phẩm dưới 5 triệu</h1>
+        <?php
+        getProductByRange(0, 4999999, $conn); 
+        ?>
     </div>
   </div>
+  <!--ads-->
+  <div class="ads">
+    <a href="#">
+      <img class="ads-img" src="../../src/Home/ads/2015.png" alt="ads" />
+    </a>
+  </div>
   <script src="./Home.js"></script>
-  <?php include "../../component/footer/footer.php" ?>
+</div>  
+<?php include "../../component/footer/footer.php" ?>
+
 </body>
 
 </html>
