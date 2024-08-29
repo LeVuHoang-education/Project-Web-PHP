a6ef13c68beb90178de9d9a7c902e3df0aaac4b0
caonhathao <73047910+caonhathao@users.noreply.github.com>
2024-08-29 13:38:41 +0700

    update

--- assets/frontend/pages/Profile/orderDetail.php
+++ assets/frontend/pages/Profile/orderDetail.php
@@ -13,21 +13,21 @@
             <table id="table-field">
                 <tr>
                     <th style="width:10%">Mã sản phẩm</th>
-                    <th style="width:30%;">Ảnh</th>
+                    <th style="width:30%;">Sản phẩm</th>
                     <th style="width:20%;">Số lượng</th>
                     <th style="width:40%;">Tổng tiền</th>
                 </tr>
                 <?php
                 if (isset($_GET['id'])) {
                     $id = $_GET['id'];
-                    $sql = "SELECT * FROM `order-detail` WHERE orderid = $id";
+                    $sql = "SELECT `order-detail`.*,product.proname FROM `order-detail` INNER JOIN product ON `order-detail`.proid = product.proid WHERE `order-detail`.orderid = $id";
                     $result = $conn->query($sql);
                     if ($result->num_rows > 0) {
                         while ($row = $result->fetch_assoc()) {
                 ?>
                             <tr>
                                 <td><?= $row['proid'] ?></td>
-                                <td>
+                                <td style="text-align:center;">
                                     <?php
                                     $id = $row['proid'];
                                     $query = "SELECT image_path FROM product WHERE proid=$id";
@@ -35,8 +35,9 @@
                                     $row_image = $data->fetch_assoc();
                                     ?>
                                     <img id="thumbnail" src=<?= $row_image['image_path'] ?>>
+                                    <p><?= $row['proname'] ?></p>
                                 </td>
-                                <td><?= $row['quantity'] ?></td>
+                                <td><?= $row['quanitity'] ?></td>
                                 <td><?= $row['price'] ?></td>
                             </tr>
                         <?php
@@ -44,10 +45,11 @@
                         ?>
             </table>
         <?php
-                    } else ?>
-        </table>
-        <p>Chưa có gì ở đây cả. Hãy mua sắm ngay!</p>
+                    } else { ?>
+            </table>
+            <p>Chưa có gì ở đây cả. Hãy mua sắm ngay!</p>
     <?php
+                    }
                 }
     ?>
         </div>
